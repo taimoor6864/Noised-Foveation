3b6ab79a4f893c4807df8f86ff6a9a31dc4a5d50
Taimoor Tariq <45258250+taimoor6864@users.noreply.github.com>
2022-08-23 17:26:10 +0200

    Delete gamma_correction.m

--- gamma_correction.m
+++
@@ -1,8 +0,0 @@
-function im_t = gamma_correction(im_in,screen_gamma,mode)
-
-if(mode == 0)
-   im_t = 1.055.*(im_in.^(1/screen_gamma)) - 0.055;
-else
-   im_t = ((im_in + 0.055)./1.055).^screen_gamma;
-end
-
